921c511ce46af93dbb19c2efcdda3801937d5acd
alena <kellnerovaajka@gmail.com>
2025-02-24 17:12:45 +0100

    Initial Commit

---
+++ SwiftSourceControl/SwiftSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSourceControlApp.swift
+//  SwiftSourceControl
+//
+//  Created by Heimdal Data on 24.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
