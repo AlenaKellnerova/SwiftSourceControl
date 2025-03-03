5403c2665d3190cbe371bbde89bb83446f9add64
alena <kellnerovaajka@gmail.com>
2025-03-03 17:24:49 +0100

    [Feature] Add ProfileView

---
+++ SwiftSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftSourceControl
+//
+//  Created by Heimdal Data on 03.03.2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
